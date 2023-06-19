ac88bec7ead1c73eed1e7c0229bbb4827f012d37
Rakesh <114426150+RakeshPuli11@users.noreply.github.com>
2023-06-19 18:50:33 +0530

    Add files via upload

---
+++ rfid.ino
@@ -0,0 +1,122 @@
+
+ /* * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15*/
+#include <SPI.h>
+#include <MFRC522.h>
+#include<Servo.h>
+Servo Myservo;
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  pinMode(7,1);
+  pinMode(6,1);
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
+
+}
+void loop() 
+{ bool flag = 0;
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "23 17 30 13") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    //delay(3000);
+    digitalWrite(6,1);
+    delay(200);
+    digitalWrite(6,0);
+    delay(200);
+    digitalWrite(6,1);
+    delay(200);
+    digitalWrite(6,0);
+    delay(200);
+    digitalWrite(6,1);
+    delay(200);
+    digitalWrite(6,0);
+    delay(1000);
+    flag = 1;
+  }
+  else if(content.substring(1) == "43 29 64 0D")
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    //delay(3000);
+    digitalWrite(6,1);
+    delay(200);
+    digitalWrite(6,0);
+    delay(200);
+    digitalWrite(6,1);
+    delay(200);
+    digitalWrite(6,0);
+    delay(200);
+    digitalWrite(6,1);
+    delay(200);
+    digitalWrite(6,0);
+    delay(1000);
+    flag = 1;
+    
+  }
+  else 
+  {
+    Serial.println(" Access denied");
+    Serial.println();
+    //delay(3000);
+    digitalWrite(7,1);
+    delay(200);
+    digitalWrite(7,0);
+    delay(200);
+    digitalWrite(7,1);
+    delay(200);
+    digitalWrite(7,0);
+    delay(200);
+    digitalWrite(7,1);
+    delay(200);
+    digitalWrite(7,0);
+    delay(1000);
+  }
+  if(flag==1){
+    int k=1;
+     while(k>0)
+     {
+      Myservo.write(0);
+      delay(1000);
+      Myservo.write(180);
+      delay(1000);
+      k--;
+     }
+  }
+ 
+ 
+} 
